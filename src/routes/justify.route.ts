3109aada1e026d1fb9ca6c2e8435f0abecd511d5
k <medo.mrt@gmail.com>
2024-03-20 10:44:35 +0100

    feat: authentication.middleware.ts added and apply to justify route

--- src/routes/justify.route.ts
+++ src/routes/justify.route.ts
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+
 import JustifyController from '../controllers/justify.controller';
+import AuthenticationMiddeware from "../middleware/authentication.middleware";
 
 const justifyController = new JustifyController()
+const authenticationMiddeware = new AuthenticationMiddeware()
 
-router.post('/', justifyController.justifyText);
+router.post('/',authenticationMiddeware.verifyJwt, justifyController.justifyText);
 
 module.exports = router
